8ab740d893e999343cbcc6beed60e696328c9df3
at12an <ahttran19@gmail.com>
2023-03-08 03:01:02 +1100

    Added sliding effect and about page

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello")
+var firstsec = document.getElementById('intro');
+var secondsec = document.getElementById('projects');
+var lastScrollTop = 0;
+window.onscroll = function(){
+   var st = window.pageYOffset || document.documentElement.scrollTop; 
+   if (st > lastScrollTop){
+      secondsec.scrollIntoView({behavior: "smooth"});
+   } else {
+      firstsec.scrollIntoView({behavior: "smooth"});
+   }
+   lastScrollTop = st <= 0 ? 0 : st; 
+} 
